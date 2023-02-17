e39fb9c6a65a078442d8e7f3fe4a8c0c223fa9d1
Gabriel Ong <ojh809@gmail.com>
2023-02-18 00:42:25 +0800

    Added POST endpoint for file uploads

--- spring-boot/src/main/java/dev/soonann/frameworkcomparison/controllers/RequestController.java
+++ spring-boot/src/main/java/dev/soonann/frameworkcomparison/controllers/RequestController.java
@@ -3,14 +3,27 @@ package dev.soonann.frameworkcomparison.controllers;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.http.HttpStatus;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 import dev.soonann.frameworkcomparison.models.*;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/transaction")
@@ -18,9 +31,7 @@ import dev.soonann.frameworkcomparison.models.*;
 public class RequestController {
 
     @Autowired
-    public RequestController() {
-
-    }
+    private HttpServletRequest request;
 
     @PostMapping
     public ResponseEntity<Double> registerRequest(@RequestBody TransactionDTO transactionDTO) {
@@ -37,9 +48,30 @@ public class RequestController {
         return new ResponseEntity<>(sum, HttpStatus.OK);
     }
 
-    // @PostMapping(path = "/file")
-    // public ResponseEntity<Object> registerFile(@RequestBody RegistrationDTO registrationDTO) {
+    @PostMapping(path = "/file")
+    public ResponseEntity<Double> registerFile(@RequestParam("file") MultipartFile file) throws FileNotFoundException, IOException, CsvValidationException {
         
-    //     return new ResponseEntity<>(responseObject, HttpStatus.CREATED);
-    // }
+        Double sum = 0.0;
+
+        // Upload File
+        File path = new File("src/main/java/dev/soonann/frameworkcomparison/uploads/" + file.getOriginalFilename());
+        path.createNewFile();
+        FileOutputStream output = new FileOutputStream(path);
+        output.write(file.getBytes());
+        output.close();
+
+        // Read File
+        // List<List<String>> records = new ArrayList<List<String>>();
+        try (CSVReader csvReader = new CSVReader(new FileReader(path))) {
+            String[] values = null;
+            while ((values = csvReader.readNext()) != null) {
+                if (values[2].equals("A")) {
+                    // records.add(Arrays.asList(values));
+                    sum += Double.parseDouble(values[5]);
+                }
+            }
+        }
+        // System.out.println(records);
+        return new ResponseEntity<>(sum, HttpStatus.CREATED);
+    }
 }
