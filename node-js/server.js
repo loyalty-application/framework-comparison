0b70861f295747b0a1409bab6a2e7f26d804e477
Jing kai Ooi <ooijingkai@gmail.com>
2023-02-17 22:03:59 +0800

    feat: transaction/file post route

--- node-js/server.js
+++ node-js/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const cors = require("cors");
-
+const fs = require('fs');
+const multer = require('multer');
+const csv = require('fast-csv');
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
+const upload = multer({ dest: 'tmp/csv/' });
+
+
 
 // const transaction = require("./routes/transaction");
 // app.use("/transaction", transaction)
@@ -20,13 +24,27 @@ app.listen(PORT, (error) => {
 app.post("/transaction", (req, res) => {
     totalAmount = 0
     for (const key in req.body["transactions"]) {
-        if(req.body["transactions"][key]["merchant"] == "A") {
+        if (req.body["transactions"][key]["merchant"] == "A") {
             totalAmount += req.body["transactions"][key]["amount"]
         }
     }
-    res.status(200).json({totalAmount})
+    res.status(200).json({ totalAmount })
 });
 
-app.post("/transaction/file", (req, res) => {
-    
+app.post("/transaction/file", upload.single('file'), async (req, res) => {
+    const fileRows = [];
+    sum = 0
+    // open uploaded file
+    await csv.parseFile(req.file.path)
+        .on("data", function (data) {
+            if (data[2] == 'A') {
+                sum += parseFloat(data[5])
+            }
+        })
+        .on("end", function () {
+            fs.unlinkSync(req.file.path);   // remove temp file
+            //process "fileRows" and respond
+            res.json({ sum });
+
+        })
 })
