f0a13d6cac2298f61a21f7a6d2a3c7ca93e7f4aa
Jing kai Ooi <ooijingkai@gmail.com>
2023-02-17 20:43:58 +0800

    feat: transaction post route

--- node-js/routes/transaction.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const {readTransactions} = require('../controllers/transactions')
-const inventoryRouter = express.Router()
-
-inventoryRouter.route('/').post(readTransactions)
--- node-js/server.js
+++ node-js/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require("cors");
 
 const app = express();
 const PORT = 8080;
 
-const transaction = require("./routes/transaction");
-app.use("/transaction", transaction)
+app.use(express.json());
+
+// const transaction = require("./routes/transaction");
+// app.use("/transaction", transaction)
 
 
 app.listen(PORT, (error) => {
@@ -12,5 +15,18 @@ app.listen(PORT, (error) => {
         console.log("Server is Successfully Running, and App is listening on port " + PORT)
     else
         console.log("Error occurred, server can't start", error);
-}
-);
+});
+
+app.post("/transaction", (req, res) => {
+    totalAmount = 0
+    for (const key in req.body["transactions"]) {
+        if(req.body["transactions"][key]["merchant"] == "A") {
+            totalAmount += req.body["transactions"][key]["amount"]
+        }
+    }
+    res.status(200).json({totalAmount})
+});
+
+app.post("/transaction/file", (req, res) => {
+    
+})
