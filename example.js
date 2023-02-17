0891f844c14386e05d35d45b743142628d4d6b27
Jing kai Ooi <ooijingkai@gmail.com>
2023-02-17 20:05:06 +0800

    added transaction route

---
+++ example.js
@@ -0,0 +1,23 @@
+[
+    {
+        "merchant": "A",
+        "amount": "2000"
+    },
+    {
+        "merchant": "A",
+        "amount": "2000"
+    },
+    {
+        "merchant": "A",
+        "amount": "2000"
+    },
+    {
+        "merchant": "B",
+        "amount": "2000"
+    },
+    {
+        "merchant": "B",
+        "amount": "2000"
+    },
+
+]
--- node-js/controllers/transactions.js
+++ node-js/controllers/transactions.js
@@ -1,6 +1,11 @@
 
 const readTransactions = (req, res) => {
-
+    for (const key in req) {
+        if (Object.hasOwnProperty.call(req, key)) {
+            const element = req[key];
+            
+        }
+    }
 }
 
 
